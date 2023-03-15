80f0b6275310b894d28bf144d136a6f899997a77
Lê Tấn Lộc <ltanloc97o@gmail.com>
2023-03-16 00:07:14 +0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -62,7 +62,7 @@ def evaluate(args):
     # cnn_trainable = bool(args.cnnTrainable)
     loss = 'binary_crossentropy'
 
-    test_generator = DataGenerator(directory = f'{dirinp}/val.pkl',
+    test_generator = DataGenerator(directory = f'{dirinp}/test.pkl',
                                 sample = False,
                                 type_part=type_part,
                                 batch_size = batch_size,
