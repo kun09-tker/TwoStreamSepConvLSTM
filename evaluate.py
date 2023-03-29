278c33c6bfd2fcbd7a03d087a8c5bc9653add035
Lê Tấn Lộc <ltanloc97o@gmail.com>
2023-03-29 19:11:09 +0700

    .

--- evaluate.py
+++ evaluate.py
@@ -74,9 +74,9 @@ def evaluate(args, data_test):
                                 mode = mode)
 
     print('> getting the model from...', resume_path) 
-    model = model_function(size=input_heatmap_size, seq_len=vid_len,cnn_trainable=False, mode=mode, frame_diff_interval=frame_diff_interval, backbone=backbone)
+    model = model_function(size=input_heatmap_size, seq_len=vid_len,cnn_trainable=True, mode=mode, frame_diff_interval=frame_diff_interval, backbone=backbone)
     optimizer = Adam(learning_rate=initial_learning_rate, amsgrad=True)
-    model.compile(optimizer=optimizer, loss=loss, metrics=["acc", f1_m]) 
+    model.compile(optimizer=optimizer, loss=loss, metrics=["acc", precision_m, recall_m, f1_m]) 
     model.load_weights(f'{currentModelPath}')
     model.trainable = False
         
